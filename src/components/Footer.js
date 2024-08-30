f2acebc7a3cfac66ccfa40bbeca220bb6b00ee4c
Goncalo Videira <goncalovideira@hotmail.com>
2024-08-30 20:24:07 +0100

    meta info and css

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="page-footer">
+      <div className="container center">
+        <div className="row">
+          © 2024 Gonçalo Videira
+        </div>
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="/smiley-app" className="brand-logo center truncate">Maria's Smiley</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
